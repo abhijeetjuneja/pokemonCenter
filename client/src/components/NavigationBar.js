e36feed49c15887ead8b547400aaf9a2f152e774
Abhijeet <abhijeetjuneja2014@gmail.com>
2018-04-09 04:48:46 +0530

    loader

--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -34,7 +34,7 @@ class NavigationBar extends React.Component {
       <strong>
         <div className="container-fluid">
           <div className="navbar-header">
-            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#navigationbar">
+            <button type="button" style={{backgroundColor:'transparent',color:'white'}} className="navbar-toggle" data-toggle="collapse" data-target="#navigationbar">
                <span className="sr-only">Toggle navigation</span>
                <span className="icon-bar"></span>
                <span className="icon-bar"></span>
